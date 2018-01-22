e7149dcbb0e7a948bfe22855ad9953251dac5172
Ahmed Tolba <tolpa1@gmail.com>
2018-01-22 22:30:54 +0100

    Add GCD

---
+++ algorithmic_toolbox_course/2nd_week/gcd/gcd.rb
@@ -0,0 +1,53 @@
+# #!/usr/bin/env ruby
+
+# Read from STDIN
+#
+n = gets.split(" ").map(&:to_i)
+#
+# if n.size != size
+#   puts "wrong input"
+#   exit
+# end
+
+def gcd(a, b)
+  best = 0
+
+  for i in (1..a+b)
+    if a % i == 0 && b % i == 0
+      best = i
+    end
+  end
+  best
+end
+
+puts gcd(n[0], n[1])
+
+# Stress test
+# while true do
+#   # Stablize the generator for debugging
+#   srand = 1000
+#
+#   size = rand(2..1000)
+#   puts size
+#
+#   n = []
+#
+#   (0..size-1).each do |i|
+#     # Let rand generate big giants shit.
+#     n << (rand * 13243240).to_i
+#   end
+#
+#   (0..size-1).each do |i|
+#     print n[i].to_s + " "
+#   end
+#
+#   res1 = test_1 # replace with first implm
+#   res2 = test_2 # replace with second implm
+#
+#   if res1 != res2
+#     puts "Wrong answer #{res1}  !=  #{res2}"
+#     break
+#   else
+#     puts "OK"
+#   end
+# end
